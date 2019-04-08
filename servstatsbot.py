ce4d64575b47693759a4760694a007a4682c1256
Dennis <den4k.savenko@gmail.com>
2019-04-09 00:43:04 +0200

    added telegram ReplyKeyboardMarkup
    and new features!

--- servstatsbot.py
+++ servstatsbot.py
@@ -5,12 +5,43 @@ import matplotlib.pyplot as plt
 import psutil
 from datetime import datetime
 from subprocess import Popen, PIPE, STDOUT
+from netaddr import IPAddress
 import operator
 import collections
 import time
 import telepot
+import nmap
+import requests
 # import sys
 # import os
+
+info_pasta ='''
+📄 Comandos:
+/info — proporciona estadísticas sumadas sobre la memoria, disk, procesos;
+/shell — entra en el modo de ejecutar comandos de shell y le envía la salida;
+/memgraph — traza un gráfico del uso de la memoria de un período pasado y le envía una imagen del gráfico;
+/setmem — establece el umbral de la memoria (%) para monitorear y notificar si el uso de la memoria está por encima;
+/setpoll — establece el intervalo de sondeo en segundos (superior a 10).'''
+
+shell_pasta ='''📝 Lista de comandos básicos disponibles:
+                     
+𝗳𝗱𝗶𝘀𝗸 -𝗹 — informacion sobre todas las unidades conectadas;
+𝘂𝗻𝗮𝗺𝗲 -𝗿 — muestra la vercion del kernel de Linux;
+𝗰𝗮𝘁 <𝘥𝘪𝘳𝘦𝘤𝘤𝘪𝘰𝘯>  — ver el contenido del archivo;
+𝗽𝘄𝗱 — mostrar directorio actual;
+𝗵𝗱𝗽𝗮𝗿𝗺 -𝗶 /𝗱𝗲𝘃/𝘀𝗱𝗮 — mostrar las especificaciones del disco duro;
+𝗽𝘀 — lista de procesos en ejecucion;
+𝘀𝗲𝗿𝘃𝗶𝗰𝗲 <𝘯𝘰𝘮𝘣𝘳𝘦 𝘥𝘦𝘭 𝘱𝘳𝘰𝘤𝘦𝘴𝘰> 𝘀𝘁𝗮𝗿𝘁/𝘀𝘁𝗼𝗽/𝗿𝗲𝘀𝘁𝗮𝗿𝘁 — realizar una accion con este servicio;
+𝗸𝗶𝗹𝗹𝗮𝗹𝗹 <𝘯𝘰𝘮𝘣𝘳𝘦 𝘥𝘦𝘭 𝘱𝘳𝘰𝘤𝘦𝘴𝘰> — matar el proceso por su nombre;
+𝗶𝗳𝘂𝗽/𝗶𝗳𝗱𝗼𝘄𝗻 𝗲𝘁𝗵𝟬 — activar/desactivar la interfaz eth0;
+𝗿𝗼𝘂𝘁𝗲 -𝗻 — mostrat la tabla de enrutamiento local;
+𝗶𝗽 𝗹𝗶𝗻𝗸 𝘀𝗵𝗼𝘄 — mostrar el estado de todas las interfaces;
+𝗼𝗽𝗸𝗴 𝘂𝗽𝗱𝗮𝘁𝗲/𝘂𝗽𝗴𝗿𝗮𝗱𝗲/𝗶𝗻𝘀𝘁𝗮𝗹𝗹 — trabajocon paquetes;
+𝘀𝗵𝘂𝘁𝗱𝗼𝘄𝗻 -𝗵 𝗻𝗼𝘄 — detener el sistema;
+𝗿𝗲𝗯𝗼𝗼𝘁 — reinicie el sistema;
+𝗻𝗺𝗮𝗽 -𝗣𝗻 -𝗔 — escanear los hosts en la red;
+
+...Más...'''
 
 memorythreshold = 85  # If memory usage more this %
 poll = 300  # seconds
@@ -23,10 +54,10 @@ settingmemth = []
 setpolling = []
 graphstart = datetime.now()
 
-stopmarkup = {'keyboard': [['Stop']]}
 hide_keyboard = {'hide_keyboard': True}
-
-markup= {'keyboard': [['/info', '/ip', '/shell', '/temperatura'], ['/setmem', '/setpoll', '/disk', '/memgraph'], ['Stop']]}
+service={'keyboard': [['start', 'restart', 'stop', 'status']]}
+markup= {'keyboard': [['/info', '/ip', '/shell', '/temperatura'], ['/disk', '/lan', "/service"], ['/setmem', '/setpoll', '/memgraph']]}
+stopmarkup = {'keyboard': [['Stop']]}
 
 def clearall(chat_id):
     if chat_id in shellexecution:
@@ -38,7 +69,6 @@ def clearall(chat_id):
 
 def bytes2human(n):
 # Credits: http://code.activestate.com/recipes/578019
-#thank you fabaff !
     symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
     prefix = {}
     for i, s in enumerate(symbols):
@@ -49,12 +79,22 @@ def bytes2human(n):
             return '%.1f%s' % (value, s)
     return "%sB" % n
 
+def disks():
+  global bytes2human
+  templ = "%-10s %8s %8s %8s \n"
+  disks = templ % ("Device", "Total", "Used", "Free")
+  for part in psutil.disk_partitions(all=False):
+      usage = psutil.disk_usage(part.mountpoint)
+      disks = disks + templ % (part.device,
+                          bytes2human(usage.total),
+                          bytes2human(usage.used),
+                          bytes2human(usage.free))
+  return str(disks)
+                                              
 def plotmemgraph(memlist, xaxis, tmperiod):
-    # print(memlist)
-    # print(xaxis)
     plt.xlabel(tmperiod)
     plt.ylabel('% Used')
-    plt.title('⚡️ Memory Usage Graph')
+    plt.title('⚡️ Memory Usage Graph ⚡️')
     plt.text(0.1*len(xaxis), memorythreshold+2, 'Threshold: '+str(memorythreshold)+ ' %')
     memthresholdarr = []
     for xas in xaxis:
@@ -65,6 +105,71 @@ def plotmemgraph(memlist, xaxis, tmperiod):
     plt.close()
     f = open('/tmp/graph.png', 'rb')  # some file on local disk
     return f
+
+def info():
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+    boottime = datetime.fromtimestamp(psutil.boot_time())
+    now = datetime.now()
+    timedif = "Online for: %.1f Hours" % (((now - boottime).total_seconds()) / 3600)
+    memtotal = "Total memory: %.2f GB " % (memory.total / 1000000000)
+    memavail = "Available memory: %.2f GB" % (memory.available / 1000000000)
+    memuseperc = "Used memory: " + str(memory.percent) + " %"
+    diskused = "Disk used: " + str(disk.percent) + " %"
+    cpupercent = "Cpu usage: " + str(psutil.cpu_percent(1)) + " %"
+    pids = psutil.pids()
+    pidsreply = ''
+    procs = {}
+    for pid in pids:
+        p = psutil.Process(pid)
+        try:
+            pmem = p.memory_percent()
+            if pmem > 0.5:
+                if p.name() in procs:
+                    procs[p.name()] += pmem
+                else:
+                    procs[p.name()] = pmem
+        except:
+            print("Hm..")
+    sortedprocs = sorted(procs.items(), key=operator.itemgetter(1), reverse=True)
+    for proc in sortedprocs:
+        pidsreply += proc[0] + " " + ("%.2f" % proc[1]) + " %\n"
+    reply = timedif + "\n" + \
+            memtotal + "\n" + \
+            memavail + "\n" + \
+            memuseperc + "\n" + \
+            diskused + "\n" + \
+            cpupercent + "\n\n" + "Top procesos:"+ "\n" + \
+            pidsreply
+    return reply
+
+def scan():
+    ip_range = "192.168.8.*" + "/" + str(IPAddress("255.255.255.0").netmask_bits())
+    try:
+        nm = nmap.PortScanner()
+        scan = nm.scan(hosts=ip_range, arguments='-sP')
+    except:
+        return "CRASH"
+    hosts = []
+    for host in scan["scan"]:
+        if "mac" in scan["scan"][host]["addresses"]:
+            if "hostnames" in scan["scan"][host] and "name" in scan["scan"][host]["hostnames"][0] and not scan["scan"][host]["hostnames"][0]["name"] == "":
+                name = scan["scan"][host]["hostnames"][0]["name"]
+                if len(name) > 15:
+                    name = name[:15] + "..."
+                hosts.append([host, scan["scan"][host]["addresses"]["mac"], name ])
+            else:
+                hosts.append([host, scan["scan"][host]["addresses"]["mac"]])
+
+    textline = "🖥️ Devices online:\n"
+    temp_latest_scan = hosts[:]
+    temp_latest_scan = sorted(temp_latest_scan, key=lambda x: x[0])
+    for host in temp_latest_scan:
+            if len(host) > 2:
+                textline += host[0] + " ➖ " + host[1] + " ➖ " + host[2] + "\n"
+            else:
+                textline += host[0] + " ➖ " + host[1] + "\n"
+    return textline
 
 class YourBot(telepot.Bot):
     def __init__(self, *args, **kwargs):
@@ -80,34 +185,30 @@ class YourBot(telepot.Bot):
             if content_type == 'text':
                 if msg['text'] == '/start' and chat_id:
                     bot.sendChatAction(chat_id, 'typing')
+                    #global info_pasta
                     bot.sendMessage(chat_id, "Hola estoy esperando a tu orden...")
                     time.sleep(1)
-                    bot.sendMessage(chat_id, text='''
-📄 Comandos:
-/info — proporciona estadísticas sumadas sobre la memoria, disk, procesos;
-/shell — entra en el modo de ejecutar comandos de shell y le envía la salida;
-/memgraph — traza un gráfico del uso de la memoria de un período pasado y le envía una imagen del gráfico;
-/setmem — establece el umbral de la memoria (%) para monitorear y notificar si el uso de la memoria está por encima;
-/setpoll — establece el intervalo de sondeo en segundos (superior a 10).''', reply_markup=markup)
+                    bot.sendMessage(chat_id, info_pasta, reply_markup=markup)
                 elif msg['text'] == "/ip" and chat_id:
                     bot.sendChatAction(chat_id, 'typing')
                     p = Popen('curl ifconfig.me', shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
                     output = p.stdout.read()
                     output = output[:-1]
                     bot.sendMessage(chat_id, str("🌍 Mi direccion ip externa : " + str(output, 'utf-8')), reply_markup=markup)
+                elif msg['text'] == "/lan" and chat_id:
+                    bot.sendChatAction(chat_id, 'typing')
+                    bot.sendMessage(chat_id, str("Scanning network... 🔎"))
+                    bot.sendChatAction(chat_id, 'typing')
+                    bot.sendMessage(chat_id, scan(), reply_markup=markup)                    
+                elif msg['text'] == "/service" and chat_id:
+                    bot.sendChatAction(chat_id, 'typing')
+                    p = "service " + msg['text']
+                    print (p)
+                    if p != b'':
+                        bot.sendMessage(chat_id, output, reply_markup=markup)
+                    bot.sendMessage(chat_id, str(output, 'utf-8'), reply_markup=markup)                 
                 elif msg['text'] == "/disk" and chat_id:
                     bot.sendChatAction(chat_id, 'typing')
-                    def disks():
-                        global bytes2human
-                        templ = "%-20s %8s %8s %8s \n"
-                        disks = templ % ("Device", "Total", "Used", "Free")
-                        for part in psutil.disk_partitions(all=False):
-                            usage = psutil.disk_usage(part.mountpoint)
-                            disks = disks + templ % (part.device,
-                                                bytes2human(usage.total),
-                                                bytes2human(usage.used),
-                                                bytes2human(usage.free))
-                        return str(disks)
                     bot.sendMessage(chat_id, disks(), reply_markup=markup)    
                 elif msg['text'] == "/temperatura" and chat_id:
                     bot.sendChatAction(chat_id, 'typing')
@@ -117,41 +218,7 @@ class YourBot(telepot.Bot):
                     bot.sendMessage(chat_id, "🔥 Temperatura de la CPU: " + str(reply) + "°C" , reply_markup=markup)
                 elif msg['text'] == '/info' and chat_id not in shellexecution:
                     bot.sendChatAction(chat_id, 'typing')
-                    memory = psutil.virtual_memory()
-                    disk = psutil.disk_usage('/')
-                    boottime = datetime.fromtimestamp(psutil.boot_time())
-                    now = datetime.now()
-                    timedif = "Online for: %.1f Hours" % (((now - boottime).total_seconds()) / 3600)
-                    memtotal = "Total memory: %.2f GB " % (memory.total / 1000000000)
-                    memavail = "Available memory: %.2f GB" % (memory.available / 1000000000)
-                    memuseperc = "Used memory: " + str(memory.percent) + " %"
-                    diskused = "Disk used: " + str(disk.percent) + " %"
-                    cpupercent = "Cpu usage: " + str(psutil.cpu_percent(1)) + " %"
-                    pids = psutil.pids()
-                    pidsreply = ''
-                    procs = {}
-                    for pid in pids:
-                        p = psutil.Process(pid)
-                        try:
-                            pmem = p.memory_percent()
-                            if pmem > 0.5:
-                                if p.name() in procs:
-                                    procs[p.name()] += pmem
-                                else:
-                                    procs[p.name()] = pmem
-                        except:
-                            print("Hm..")
-                    sortedprocs = sorted(procs.items(), key=operator.itemgetter(1), reverse=True)
-                    for proc in sortedprocs:
-                        pidsreply += proc[0] + " " + ("%.2f" % proc[1]) + " %\n"
-                    reply = timedif + "\n" + \
-                            memtotal + "\n" + \
-                            memavail + "\n" + \
-                            memuseperc + "\n" + \
-                            diskused + "\n" + \
-                            cpupercent + "\n\n" + "Top procesos:"+ "\n" + \
-                            pidsreply
-                    bot.sendMessage(chat_id, reply, reply_markup=markup)
+                    bot.sendMessage(chat_id, "ℹ️ Informacion: \n" + info(), reply_markup=markup)
                 elif msg['text'] == "Stop":
                     clearall(chat_id)
                     bot.sendMessage(chat_id, "⛔️ Todas las operaciones se detuvieron.", reply_markup=markup)
@@ -172,25 +239,8 @@ class YourBot(telepot.Bot):
                     except:
                         bot.sendMessage(chat_id, "Por favor envíe un valor numérico apropiado mayor que 10:")
                 elif msg['text'] == "/shell" and chat_id not in shellexecution:
-                    bot.sendMessage(chat_id, text='''📝 Lista de comandos básicos disponibles:
-                     
-𝗳𝗱𝗶𝘀𝗸 -𝗹 — informacion sobre todas las unidades conectadas;
-𝘂𝗻𝗮𝗺𝗲 -𝗿 — muestra la vercion del kernel de Linux;
-𝗰𝗮𝘁 <𝘥𝘪𝘳𝘦𝘤𝘤𝘪𝘰𝘯>  — ver el contenido del archivo;
-𝗽𝘄𝗱 — mostrar directorio actual;
-𝗵𝗱𝗽𝗮𝗿𝗺 -𝗶 /𝗱𝗲𝘃/𝘀𝗱𝗮 — mostrar las especificaciones del disco duro;
-𝗽𝘀 — lista de procesos en ejecucion;
-𝘀𝗲𝗿𝘃𝗶𝗰𝗲 <𝘯𝘰𝘮𝘣𝘳𝘦 𝘥𝘦𝘭 𝘱𝘳𝘰𝘤𝘦𝘴𝘰> 𝘀𝘁𝗮𝗿𝘁/𝘀𝘁𝗼𝗽/𝗿𝗲𝘀𝘁𝗮𝗿𝘁 — realizar una accion con este servicio;
-𝗸𝗶𝗹𝗹𝗮𝗹𝗹 <𝘯𝘰𝘮𝘣𝘳𝘦 𝘥𝘦𝘭 𝘱𝘳𝘰𝘤𝘦𝘴𝘰> — matar el proceso por su nombre;
-𝗶𝗳𝘂𝗽/𝗶𝗳𝗱𝗼𝘄𝗻 𝗲𝘁𝗵𝟬 — activar/desactivar la interfaz eth0;
-𝗿𝗼𝘂𝘁𝗲 -𝗻 — mostrat la tabla de enrutamiento local;
-𝗶𝗽 𝗹𝗶𝗻𝗸 𝘀𝗵𝗼𝘄 — mostrar el estado de todas las interfaces;
-𝗼𝗽𝗸𝗴 𝘂𝗽𝗱𝗮𝘁𝗲/𝘂𝗽𝗴𝗿𝗮𝗱𝗲/𝗶𝗻𝘀𝘁𝗮𝗹𝗹 — trabajocon paquetes;
-𝘀𝗵𝘂𝘁𝗱𝗼𝘄𝗻 -𝗵 𝗻𝗼𝘄 — detener el sistema;
-𝗿𝗲𝗯𝗼𝗼𝘁 — reinicie el sistema;
-𝗻𝗺𝗮𝗽 -𝗣𝗻 -𝗔 — escanear los hosts en la red;
-...Más...
-    ''')
+                    #global info_pasta
+                    bot.sendMessage(chat_id, shell_pasta)
                     time.sleep(1)
                     bot.sendMessage(chat_id, "✍️ Envíame un comando de 𝘀𝗵𝗲𝗹𝗹 para ejecutar:", reply_markup=stopmarkup)
                     shellexecution.append(chat_id)
@@ -216,9 +266,9 @@ class YourBot(telepot.Bot):
                     p = Popen(msg['text'], shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
                     output = p.stdout.read()
                     if output != b'':
-                        bot.sendMessage(chat_id, output, reply_markup=markup)
+                        bot.sendMessage(chat_id, output, reply_markup=stopmarkup)
                     else:
-                        bot.sendMessage(chat_id, "❌ No hay datos de salida.", reply_markup=markup)
+                        bot.sendMessage(chat_id, "❌ No hay datos de salida.", reply_markup=stopmarkup)
                 elif msg['text'] == '/memgraph':
                     bot.sendChatAction(chat_id, 'typing')
                     tmperiod = "Last %.2f hours" % ((datetime.now() - graphstart).total_seconds() / 3600)
